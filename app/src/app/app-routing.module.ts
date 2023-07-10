cf42e08fdd45962c7291dca01a1cb5b337e86467
Christian <christian.di.michele2@gmail.com>
2023-07-10 15:37:54 +0200

    configurato routing di base

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "" , component:LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
