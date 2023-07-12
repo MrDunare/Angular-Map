c614e71592dbc504326f0b3883a803ca6dae9599
Christian <christian.di.michele2@gmail.com>
2023-07-12 12:03:20 +0200

    leaflet dependencies added, added input section and map in the map-component on map-branch

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { CatFactComponent } from './components/cat-fact/cat-fact.component';
+import { MapComponent } from './components/map/map.component';
 
 
 const routes: Routes = [
   {path: "" , component:LandingComponent},
   {path: "catfacts" , component:CatFactComponent},
+  {path: "map" , component:MapComponent},
 ];
 
 @NgModule({
--- app/src/app/components/map/map.component.ts
+++ app/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class MapComponent {
 
+  city!:string; 
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  initMap() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    
+  }
 }
+
