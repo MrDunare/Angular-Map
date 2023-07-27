4418936ee049fa10f447d7ab2abc21e3de852763
Elena Sasha <sasha9107@libero.it>
2023-07-27 11:30:47 +0200

    implementing graph

--- app/src/app/components/beer-graph/beer-graph.component.ts
+++ app/src/app/components/beer-graph/beer-graph.component.ts
@@ -11,27 +11,87 @@ import { GraphService } from 'src/app/services/graph.service';
 export class BeerGraphComponent {
   numBeer!: number;
   beerList: Beer[] = [];
-  data: string[] = [];
+  beerName: string[] = [];
   volumes: string[] = [];
+  isVisible: boolean = false
 
   constructor(private graphServices: GraphService) {}
 
   Highcharts: typeof Highcharts = Highcharts;
   updateFlag = false;
 
-  findGraph() {
+  getValues() {
     this.graphServices.generateUrl(this.numBeer);
     this.graphServices.getData().subscribe((data) => {
       this.beerList = data;
+      console.log(data);
+  
+      let seriesData: number[] = [];
+  
+      this.beerList.forEach((element) => {
+        this.beerName.push(element.name);
+        this.volumes.push(element.alcohol.slice(0, -1));
+        seriesData.push(parseFloat(element.alcohol.slice(0, -1)));
+      });
+  
+      this.chartOptions = {
+        title: {
+          text: 'Beer Graph',
+        },
+        series: [
+          {
+            type: 'column',
+            name: 'Volume Alcohol',
+            data: seriesData,
+          },
+        ],
+        xAxis: {
+          title: {
+            text: 'Name Beer',
+          },
+          categories: this.beerName,
+        },
+        yAxis: {
+          title: {
+            text: 'alcohol content',
+          },
+        },
+      };
+  
+      this.updateFlag = true;
+      this.isVisible = true
     });
   }
-
+  
   chartOptions: Highcharts.Options = {
+    title: {
+      text: 'Beer Graph',
+    },
+
     series: [
       {
         type: 'column',
-        data: this.data,
+        name: 'Volume Alcohol',
+        data: this.volumes,
       },
     ],
+    xAxis: {
+      title: {
+        text: 'Name Beer',
+      },
+      categories: this.beerName,
+    },
+    yAxis: {
+      title: {
+        text: 'alcohol content',
+      },
+    },
   };
 }
+
+
+
+
+
+
+
